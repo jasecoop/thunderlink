dd2254bfa86119b4af1fdc5fe648d2e3c7b86577
Jason Cooper <jase.coop@gmail.com>
2013-05-24 15:50:48 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thunderlink::Application.config.secret_token = '4241a2b16a3e1bd63f0528fb1d9e4f41157df4c68fdbff2ade077fa444a6d7fa242e556c9e767c7ca695798c6e58c8f26ff1ccfa17a63d1614a2f252b791f190'
